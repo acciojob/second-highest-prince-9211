abef4c4cbba92d32e27c5cff64cc5a9464b89685
prince kumar sacxena <86111082+prince-9211@users.noreply.github.com>
2025-07-09 13:53:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+	arr=arr.map(Number);
+	const unique=[...new set(arr)];
+	if(unique.length<2)return - Infinity;
+ unique.sort((a, b) => b - a);
+  return unique[1];}
